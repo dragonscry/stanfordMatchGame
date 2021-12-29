638d5d6d9794c2a2528743701b2b82e807461dfe
dragonscry <denismerkotun@gmail.com>
2021-12-29 23:01:58 +0200

    first steps, begin 2nd lecture of standford course

--- StandfordMatchGame/StandfordMatchGame/ContentView.swift
+++ StandfordMatchGame/StandfordMatchGame/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojies = ["🚘","🚔","🚅","✈️"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<emojies.count){ value in
+                CardView(text: emojies[value])
+            }
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct CardView: View {
+    
+    let text : String
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("\(text)")
+                .font(.largeTitle)
+        }
     }
 }
