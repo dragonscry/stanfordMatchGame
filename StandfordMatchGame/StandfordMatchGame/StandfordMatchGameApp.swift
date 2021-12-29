8aa89f5065cc3a138cd9dc09ae0b11eb57b7c20c
dragonscry <denismerkotun@gmail.com>
2021-12-29 13:18:16 +0200

    add proj

---
+++ StandfordMatchGame/StandfordMatchGame/StandfordMatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandfordMatchGameApp.swift
+//  StandfordMatchGame
+//
+//  Created by Denys on 29.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct StandfordMatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
