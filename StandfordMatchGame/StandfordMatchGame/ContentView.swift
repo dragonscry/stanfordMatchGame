258255c6c2d66a113364e09e28c32ec846266e5d
dragonscry <denismerkotun@gmail.com>
2021-12-30 20:46:16 +0200

    grid view and ascpect ratio

--- StandfordMatchGame/StandfordMatchGame/ContentView.swift
+++ StandfordMatchGame/StandfordMatchGame/ContentView.swift
@@ -9,16 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let emojies = ["🚘","🚔","🚅","✈️"]
+    let emojies = ["🚘","🚔","🚅","✈️","🦼", "🛴", "🛵",
+                   "🚲", "🛺", "🚍", "🚡", "🚠", "🚃", "🚝", "🛩", "🛰"]
+    
+    @State var emojiCount = 4
     
     var body: some View {
-        HStack {
-            ForEach(0..<emojies.count){ value in
-                CardView(text: emojies[value])
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                    ForEach(emojies[0..<emojiCount], id: \.self){ value in
+                        CardView(text: value).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
             }
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+            
         }
         .padding(.horizontal)
         .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button(action: {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        }){
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button(action: {
+            if emojiCount < emojies.count {
+                emojiCount += 1
+            }
+        }){
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
@@ -33,12 +69,22 @@ struct CardView: View {
     
     let text : String
     
+    @State var faceUp = false
+    
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("\(text)")
-                .font(.largeTitle)
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if faceUp {
+                shape.fill(.white)
+                shape.stroke(lineWidth: 3)
+                Text("\(text)")
+                    .font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            faceUp.toggle()
         }
     }
 }
