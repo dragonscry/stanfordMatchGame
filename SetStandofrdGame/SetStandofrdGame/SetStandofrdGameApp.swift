3e8804c47c839aff8d60a7bcc3db6117d880b344
dragonscry <denismerkotun@gmail.com>
2022-01-08 17:53:22 +0200

    new game Set from scratch

---
+++ SetStandofrdGame/SetStandofrdGame/SetStandofrdGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetStandofrdGameApp.swift
+//  SetStandofrdGame
+//
+//  Created by Denys on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SetStandofrdGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
