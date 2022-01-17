5b16ee8d9c45ddf5c29bdcab7235477ce7812892
dragonscry <denismerkotun@gmail.com>
2022-01-17 18:04:25 +0200

    add animation

--- StandfordMatchGame/StandfordMatchGame/ContentView.swift
+++ StandfordMatchGame/StandfordMatchGame/ContentView.swift
@@ -43,22 +43,28 @@ struct ContentView_Previews: PreviewProvider {
 }
 struct CardView: View {
     let card: MemoryGame<String>.Card
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 Pie(startAngle: Angle(degrees: 30), endAngle: Angle(degrees: -90)).padding(5).opacity(0.5)
                 Text("\(card.content)")
-                    .font(font(in: geometry.size))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(.linear(duration: 1).repeatForever(autoreverses: false))
+                    .font(Font.system(size: DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits: geometry.size))
+                
             }
             .cardify(isFaceUp: card.isFaceUp)
         }
     }
     
-    private func font(in size: CGSize) -> Font {
-        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
     }
     
     private struct DrawingConstants {
         static let fontScale: CGFloat = 0.7
+        static let fontSize: CGFloat = 32
     }
 }
