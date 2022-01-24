fd078e91eb890838910f2d68ed5c5e2c9873d94b
dragonscry <denismerkotun@gmail.com>
2022-01-24 19:51:16 +0200

    new proj

---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Denys on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
