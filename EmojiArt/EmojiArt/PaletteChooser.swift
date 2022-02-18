450d682663748a2e621b9f23a7e6fa1bb27411e2
dragonscry <denismerkotun@gmail.com>
2022-02-18 22:03:45 +0200

    make comment to implement removing Duplicates

--- EmojiArt/EmojiArt/PaletteChooser.swift
+++ EmojiArt/EmojiArt/PaletteChooser.swift
@@ -47,7 +47,7 @@ struct ScrollingEmojisView: View {
     var body: some View {
         ScrollView(.horizontal) {
             HStack {
-                ForEach(emojis.removingDuplicateCharacters.map {String($0) }, id: \.self) { emoji in
+                ForEach(emojis.removingDuplicateCharacters.map {String($0) }, id: \.self) { emoji in //need to implement
                     Text(emoji).onDrag {
                         NSItemProvider(object: emoji as NSString)
                     }
