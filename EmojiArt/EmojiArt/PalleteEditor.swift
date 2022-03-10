42963d134917c873602b3f9518538515379ca72b
dragonscry <denismerkotun@gmail.com>
2022-03-10 23:12:05 +0200

    another red alert siren

--- EmojiArt/EmojiArt/PalleteEditor.swift
+++ EmojiArt/EmojiArt/PalleteEditor.swift
@@ -12,15 +12,17 @@ struct PalleteEditor: View {
     
     var body: some View {
         Form {
-            TextField("Name", text: $palette.name)
+            Section(header: Text("Name")){
+                TextField("Name", text: $palette.name)
+            }
         }
         .frame(minWidth: 300, minHeight: 350)
     }
 }
 
-//struct PalleteEditor_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PalleteEditor()
-//            .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/350.0/*@END_MENU_TOKEN@*/))
-//    }
-//}
+struct PalleteEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        PalleteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 4)))
+            .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/350.0/*@END_MENU_TOKEN@*/))
+    }
+}
