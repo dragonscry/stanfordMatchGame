b130b99bfbc0b538113bb2b564f3f5627e2bf0ae
dragonscry <denismerkotun@gmail.com>
2022-03-11 23:40:22 +0200

    add emoji by context menu

--- EmojiArt/EmojiArt/PalleteEditor.swift
+++ EmojiArt/EmojiArt/PalleteEditor.swift
@@ -12,11 +12,36 @@ struct PalleteEditor: View {
     
     var body: some View {
         Form {
-            Section(header: Text("Name")){
-                TextField("Name", text: $palette.name)
-            }
+            nameSection
+            addEmojisSection
         }
         .frame(minWidth: 300, minHeight: 350)
+    }
+    
+    var nameSection: some View {
+        Section(header: Text("Name")){
+            TextField("Name", text: $palette.name)
+        }
+    }
+    @State private var emojisToAdd = ""
+    
+    var addEmojisSection: some View {
+        Section(header: Text("Add Emojis")){
+            TextField("", text: $emojisToAdd)
+                .onChange(of: emojisToAdd) {
+                    emojis in
+                    addEmojis(emojis)
+                }
+        }
+    }
+    
+    func addEmojis(_ emojis: String) {
+        withAnimation {
+            palette.emojis = (emojis + palette.emojis)
+                .filter {$0.isEmoji}
+                .removingDuplicateCharacters
+        }
+
     }
 }
 
